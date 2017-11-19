8ecf7e05dac7f1bf87d432c504f5f1b7b086fb32
Derek <dereklee@downtothis.com>
2017-11-19 10:13:04 +0900

    Make first test pass with setting logon button text.

--- UIButtonDecouple/UI/LogonViewController.swift
+++ UIButtonDecouple/UI/LogonViewController.swift
@@ -3,13 +3,23 @@ import UIKit
 class LogonViewController: UIViewController {
     let logonButton: UIButton!
 
+    // MARK: - Initialization
     init() {
         logonButton = UIButton(type: .system)
 
         super.init(nibName: nil, bundle: nil)
+
+        configureViews()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - View Setup
+fileprivate extension LogonViewController {
+    func configureViews() {
+        logonButton.setTitle("Logon", for: .normal)
     }
 }
