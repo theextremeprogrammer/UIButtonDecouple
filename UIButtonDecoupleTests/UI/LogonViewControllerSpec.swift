869da1a85bf4187812daac4f1e74da9f45ba6832
Derek <dereklee@downtothis.com>
2017-11-21 07:51:28 +0900

    Update findButton() to be explicit functional instead of procedural.

--- UIButtonDecoupleTests/UI/LogonViewControllerSpec.swift
+++ UIButtonDecoupleTests/UI/LogonViewControllerSpec.swift
@@ -42,21 +42,14 @@ extension UIViewController {
 
 extension UIView {
     func findButton(withText searchText: String) -> UIButton? {
-        for subview in subviews {
-            if let button = subview as? UIButton {
-                if button.titleLabel?.text == searchText {
-                    return button
-                }
+        return subviews
+            .flatMap { subview -> UIButton? in
+                return subview as? UIButton ?? subview.findButton(withText: searchText)
             }
-
-            if subview.subviews.count > 0 {
-                if let button = subview.findButton(withText: searchText) {
-                    return button
-                }
+            .filter { button -> Bool in
+                return button.titleLabel?.text == searchText
             }
-        }
-
-        return nil
+            .first
     }
 }
 
